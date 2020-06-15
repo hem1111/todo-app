21046133154b13c110a08eb455210b535be05487
Hem <hemdesigner1111@gmail.com>
2020-06-15 19:43:34 +0545

    My first commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <Todos v-bind:todos="todos" />    
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos'
+
+export default {
+  name: 'App',
+  components: {
+    Todos
+  },
+  data(){
+    return{
+      todos: [
+        {
+          id: 1,
+          title: "number 1",
+          completed: true
+        },
+        {
+          id: 2,
+          title: "number 2",
+          completed: true
+        },
+        {
+          id: 1,
+          title: "number 3",
+          completed: false
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body{
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+</style>
